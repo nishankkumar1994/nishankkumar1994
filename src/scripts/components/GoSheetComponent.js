61e0e134982e3d7484af76b9e693e7e50abcdbbe
nishankkumar1994 <nishank.kumar@joshtechnologygroup.com>
2018-02-25 19:24:36 +0530

    added all tsba

--- src/scripts/components/GoSheetComponent.js
+++ src/scripts/components/GoSheetComponent.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactGoogleSheetConnector from "react-google-sheet-connector";
 import { YOUR_CLIENT_ID, YOUR_API_KEY, YOUR_SPREADSHEET_ID } from '../Constant/index';
 import GoSheetData from './GoSheetData';
+import { Tabs, Tab, TabContainer, TabContent, TabPane } from 'react-bootstrap';
 
 class GoSheetComponent extends React.Component {
   constructor(props) {
     super(props)
   }
-
+  handle (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  }
   render() {
     return (
       <div>
@@ -15,8 +19,25 @@ class GoSheetComponent extends React.Component {
           apiKey={YOUR_API_KEY}
           spreadsheetId={YOUR_SPREADSHEET_ID}
           spinner={ <div className="loading-spinner"/> } >
-            <div>
-              <GoSheetData />
+            <div className="nk-override-tab">
+              <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+                <Tab eventKey={1} title="HTML">
+                  <GoSheetData tabName="HTML" />
+                </Tab>
+                <Tab eventKey={2} title="CSS">
+                  <GoSheetData tabName="CSS" />
+                </Tab>
+                <Tab eventKey={3} title="JS">
+                  <GoSheetData tabName="JS" />
+                </Tab>
+                <Tab eventKey={4} title="Jquery">
+                  <GoSheetData tabName="Jquery" />
+                </Tab>
+                <Tab eventKey={5} title="Coding">
+                  <GoSheetData tabName="Coding" />
+                </Tab>
+              </Tabs>
+
             </div>
         </ReactGoogleSheetConnector>
       </div>
--- src/scripts/components/GoSheetData.js
+++ src/scripts/components/GoSheetData.js
@@ -5,7 +5,7 @@ const GoSheetData = (props) => {
     return (
         <div>
             {
-                props.getSheet("HTML")
+                props.tabName && props.getSheet(props.tabName)
                   .map((row, i) =>
                     <div className="card-wrap" key={`kjhgf${i}`}>
                       <div className="card-que"><span>{i+1}</span>Que: {row[0]}</div>
@@ -15,7 +15,7 @@ const GoSheetData = (props) => {
                         <div className="card-author-wrap"><span className="card-author-label">Author:</span> {row[3]}</div>
                       </div>
                     </div>
-                  )
+                )
             }        
         </div>
     )
